97657faa874161afc8efd541749625868098a6d9
pwgreco <paul.greco1995@gmail.com>
2019-03-07 21:52:14 -0500

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -34,7 +34,7 @@
 		 * Background image
 		/* ---------------------------------------------- */
 
-		$('#intro').backstretch(['assets/images/bg2.jpg']);
+		$('#intro').backstretch(['assets/images/header.png']);
 
 		/* ---------------------------------------------- /*
 		 * Navbar
